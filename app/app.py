f383cc189d633beb19ba0a1ad534c45c09d1d24d
Gui Utko <utko.guilherme@gmail.com>
2024-12-15 21:20:44 -0300

    start option 3

--- app/app.py
+++ app/app.py
@@ -113,4 +113,6 @@ if __name__ == '__main__':
             if db.session:
                 db.session.rollback()
     
-    app.run(debug=True, port=5001)
+    # Ajuste do host e porta para compatibilidade com Render.com
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
